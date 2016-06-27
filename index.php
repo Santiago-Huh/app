323604f840771548194ddcbdaab7d26f26fa9972
Santiago Huh <ayrancan619@gmail.com>
2016-06-26 23:23:16 -0500

    Repositorio inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aplicación</title>
+</head>
+<body>
+	<h1>Página Principal</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+	<h2>Encabezado tipo H2</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</body>
+</html>
